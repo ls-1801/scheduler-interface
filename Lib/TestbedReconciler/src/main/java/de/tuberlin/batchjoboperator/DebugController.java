f7211fdecec2fe13203160d75718f1eafbf2cffe
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2022-03-29 23:38:07 +0200

    Reset Failed Batchjobs

--- Lib/TestbedReconciler/src/main/java/de/tuberlin/batchjoboperator/DebugController.java
+++ Lib/TestbedReconciler/src/main/java/de/tuberlin/batchjoboperator/DebugController.java
@@ -2,10 +2,13 @@ package de.tuberlin.batchjoboperator;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.tuberlin.batchjoboperator.common.crd.batchjob.BatchJob;
+import de.tuberlin.batchjoboperator.common.crd.batchjob.BatchJobState;
 import de.tuberlin.batchjoboperator.common.crd.slots.Slot;
 import de.tuberlin.batchjoboperator.testbedreconciler.ClusterAllocatableResources;
 import de.tuberlin.batchjoboperator.testbedreconciler.ClusterAvailableResources;
 import de.tuberlin.batchjoboperator.testbedreconciler.ClusterRequestedResources;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.api.model.Quantity;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
@@ -28,6 +31,31 @@ public class DebugController {
     private final KubernetesClient client;
     private final ObjectMapper mapper;
     private final String namespace;
+
+    @GetMapping(value = "/debug/reset-all-jobs")
+    public List<String> resetAllJobs(@RequestParam("allNamespaces") boolean allNamespaces) {
+        var namespacedClient = allNamespaces ? client.resources(BatchJob.class) :
+                client.resources(BatchJob.class).inNamespace(namespace);
+
+        return namespacedClient.list().getItems().stream()
+                               .filter(job -> job.getStatus().getState() == BatchJobState.FailedState)
+                               .map(job -> {
+                                   namespacedClient.delete(job);
+                                   var newJob = new BatchJob();
+                                   newJob.setSpec(job.getSpec());
+                                   newJob.setMetadata(new ObjectMetaBuilder()
+                                           .withName(job.getMetadata().getName())
+                                           .withNamespace(job.getMetadata().getNamespace())
+                                           .build()
+                                   );
+                                   namespacedClient.create(newJob);
+
+                                   return newJob.getMetadata().getName();
+                               })
+                               .collect(Collectors.toList());
+
+
+    }
 
     @GetMapping(value = "/debug/node-set-up")
     public List<String> setupLabelsOnNode(
